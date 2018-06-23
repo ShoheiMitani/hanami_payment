fce4f74528d50b750114e8c2ee464eee1e6e7567
ShoheiMitani <mitani49@gmail.com>
2018-06-23 14:29:15 +0900

    Added creating item function

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -3,4 +3,11 @@
 #
 # Example:
 # get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
-get '/items', to: 'items#index'
+
+# resourcesにしないとnew_items_pathってerbにかけない
+# get '/items', to: 'items#index'
+# get '/items/new', to: 'items#new'
+# post '/items', to: 'items#create'
+
+root to: 'items#index'
+resources :items, only: %i[index new create]
