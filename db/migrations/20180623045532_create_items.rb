9d51fdeb8640178399c4740aadc47a4abec5b7e8
ShoheiMitani <mitani49@gmail.com>
2018-06-23 15:47:03 +0900

    utf-8に戻す その2

--- db/migrations/20180623045532_create_items.rb
+++ db/migrations/20180623045532_create_items.rb
@@ -1,6 +1,6 @@
 Hanami::Model.migration do
   change do
-    create_table :items, options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci' do
+    create_table :items do
       primary_key :id
 
       column :title,  String, null: false
